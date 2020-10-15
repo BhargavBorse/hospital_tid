54e9a6915ae6757f4cc36f6e6cbe31f319432c08
Bhargav Borse <bhargavborse4898@gmail.com>
2020-10-15 21:08:09 +0530

    final commit with validation

--- assets/js_files/signout.js
+++
@@ -1,7 +0,0 @@
-function signOut() {
-    firebase.auth().signOut().then(function () {
-        window.location.replace('index.html');
-    }).catch(function (error) {
-        // An error happened.
-    });
-}
